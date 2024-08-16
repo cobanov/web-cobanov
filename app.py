8de587e7764a288b05c88c9e36cb484092c9d967
mertcobanov <mertcobanovv@gmail.com>
2024-08-16 18:27:48 +0300

    refactor: Remove unused code and update routing in app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import os
 app = Flask(__name__)
 
 
-# Function to retrieve blog posts and their titles
 def get_blog_posts():
     blog_dir = os.path.join(app.root_path, "templates", "blog_posts")
     blog_files = [f for f in os.listdir(blog_dir) if f.endswith(".html")]
@@ -14,10 +13,8 @@ def get_blog_posts():
         "generative_art": "Generative Art: A Gentle Introduction",
         "vision_transformers": "Vision Transformers: A New Era in Computer Vision",
         "what_is_generative_art": "What is Generative Art?",
-        # Add more mappings as needed
     }
 
-    # Filtering to only include files that exist
     blog_posts = {
         key: blog_posts[key] for key in blog_posts if f"{key}.html" in blog_files
     }
@@ -31,9 +28,9 @@ def home():
     return render_template("index.html", posts=blog_posts)
 
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+# @app.route("/about")
+# def about():
+#     return render_template("about.html")
 
 
 @app.route("/projects")
@@ -41,9 +38,9 @@ def projects():
     return render_template("projects.html")
 
 
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
+# @app.route("/contact")
+# def contact():
+#     return render_template("contact.html")
 
 
 @app.route("/blog")
